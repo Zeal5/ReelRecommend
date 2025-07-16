cd527bb1fbaaf886720704e1495d0972aae8e65d
Zeal5 <tassadiqshakeel5@gmail.com>
2025-07-16 22:32:29 +0500

    create global constants for backend urls

---
+++ frontend/src/data/constants.tsx
@@ -0,0 +1,11 @@
+export const BASE_URL = "http://localhost:8000";
+
+
+export const BACKEND_ENDPOINTS = {
+
+  REGISTER: BASE_URL + "/register/",
+  // LOGIN: BASE_URL + "/api/login",
+  // GET_MOVIES: BASE_URL + "/api/movies",
+  // GET_MOVIE_DETAILS: BASE_URL + "/api/movie/",
+
+}
