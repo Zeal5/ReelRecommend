08c9187241f09a23d2106095a179399da23d0fc2
Zeal5 <tassadiqshakeel5@gmail.com>
2025-07-19 21:58:50 +0500

    add custom UserAdmin interface

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,52 @@
 from django.contrib import admin
-from users.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
-admin.site.register(User)
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Fields to display in the users listing page
+    list_display = ("username", "email", "is_superuser")
+
+    # Fields available for search bar
+    search_fields = ("username", "email")
+
+    # Ordering the users list
+    ordering = ("username",)
+
+    # add field set for user creation
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("username", "last_name")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+    search_fields = ("email", "username", "last_name")
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
